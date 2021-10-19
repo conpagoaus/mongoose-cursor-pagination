2070f37815872f667ec24649abc0958931e1c7e0
Emanuele Tonello <emanueletonello@gmail.com>
2021-10-19 15:08:22 +1000

    updated dependencies and mongoose to v6

--- tests/config/setup.js
+++ tests/config/setup.js
@@ -1,35 +1,30 @@
-const {
-  MongoMemoryServer
-} = require('mongodb-memory-server')
-const mongoose = require('mongoose')
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
 
-require('./models')
+require('./models');
 
-let mongoServer
+let mongoServer;
 
 beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create()
-  await mongoose.connect(mongoServer.getUri(), {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-      useUnifiedTopology: true
-  })
-})
+  mongoServer = await MongoMemoryServer.create();
+  await mongoose.connect(mongoServer.getUri(), {});
+});
 
 afterAll(async () => {
-   await mongoose.disconnect();
+  await mongoose.disconnect();
 });
 
 // Empty the database before each test
 beforeEach(async () => {
   await Promise.all(
-    Object.values(mongoose.connection.collections).map(collection => (
-         new Promise((resolve, reject) => {
-            collection.deleteMany((err) => {
-          if (err) return reject(err)
-          resolve()
+    Object.values(mongoose.connection.collections).map(
+      (collection) =>
+        new Promise((resolve, reject) => {
+          collection.deleteMany((err) => {
+            if (err) return reject(err);
+            resolve();
+          });
         })
-      })
-    ))
-  )
-})
+    )
+  );
+});
